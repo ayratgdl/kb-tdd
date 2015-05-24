3e4a9d2275731e9a2ea098e6d1a98a8ce9760201
Ayrat Gadelshin <ayrat@fib.pw>
2015-05-24 13:32:49 +0300

    Глава1. Создаём класс Dollar.

---
+++ src/main/java/pw/fib/kbtdd/Dollar.java
@@ -0,0 +1,13 @@
+package pw.fib.kbtdd;
+
+public class Dollar {
+    public int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
