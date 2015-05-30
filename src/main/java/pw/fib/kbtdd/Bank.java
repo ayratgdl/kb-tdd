877eaeb7e83c8128738f7f8f794429f7f2a22fd2
Ayrat Gadelshin <ayrat@fib.pw>
2015-05-30 23:15:12 +0300

    Глава 12. Начали реализовывать функционал сложения валют. Создали интерфейс Expression.

---
+++ src/main/java/pw/fib/kbtdd/Bank.java
@@ -0,0 +1,7 @@
+package pw.fib.kbtdd;
+
+public class Bank {
+    public Money reduce(Expression source, String currency) {
+        return Money.dollar(10);
+    }
+}
---
+++ src/main/java/pw/fib/kbtdd/Expression.java
@@ -0,0 +1,4 @@
+package pw.fib.kbtdd;
+
+public interface Expression {
+}
